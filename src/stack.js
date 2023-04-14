abb61bd22e95f68eed10f149a16d9c4447cda79e
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-14 21:37:08 +0300

    feat: описать СД stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  
+  push(element) {
+    const node = {
+      element,
+      next: null,
+    }
+    
+    if (this.head) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      this.head = node;
+    }
+    
+    this.length++;
+    
+    return this;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head;
+
+    this.head = this.head.next;
+    this.length--;
+
+    return current.element;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head.element;
   }
 }
 
